72a898594b06246ea81f9da88ec9a05edbe1f66e
Miracleoluebube <98972025+Miracleoluebube@users.noreply.github.com>
2022-06-24 01:05:09 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+
+})
+
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Freelancer", "Front-end Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+/* hide menu */
+closeBtn.addEventListener('click', ()=> {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+  
+})
+
+const navItems = menu.querySelectorAll('li');
+const changeActiveItem = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a'); 
+        link.classList.remove('active');
+    })
+}
+
+
+
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        changeActiveItem();
+link.classList.add('active');
+    })
+})
+
+//read- more
+const readMoreBtn = document.querySelector('.read-more');
+const readMoreContent = document.querySelector('.read-more-content');
+
+readMoreBtn.addEventListener('click', () => {
+    readMoreContent.classList.toggle('show-content');
+    if(readMoreContent.classList.contains('show-content')) {
+        readMoreBtn.textContent = "show-less";
+    } else {
+        readMoreBtn.textContent = "show-more";
+    }
+})
+
+// show/hide skills items 
+const skillItems = document.querySelectorAll('section.skills .skill');
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
+        skill.querySelector('.items').classList.toggle('show-items')
+    })
+})
+
+//add box shadow scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
+})
